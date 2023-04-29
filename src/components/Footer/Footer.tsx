838058e203047d4edb8e1ed8afddca7f7d0c97a7
Ritik2712 <ritik27122mahajan@gmail.com>
2023-04-29 22:41:31 +0530

    footer changed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -24,10 +24,10 @@ export default function Footer(): ReactElement {
           <Button
             style="text"
             size="small"
-            href="https://oceanprotocol.com"
+            href="https://video.wiki"
             target="_blank"
           >
-            Ocean Protocol <External className={styles.svg} />
+            VideoWiki <External className={styles.svg} />
           </Button>
         </div>
       </div>
